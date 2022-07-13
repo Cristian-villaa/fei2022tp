52d8dad8108d90bf7cac3b8613dd54456e652312
Cristian-villaa <cristianporras.v@gmail.com>
2022-07-13 20:58:28 -0300

    TP-3 (5) Creación de las APIs

--- backend/basic/modules/apiv1/models/ReservaAula.php
+++ backend/basic/modules/apiv1/models/ReservaAula.php
@@ -5,7 +5,7 @@ namespace app\modules\apiv1\models;
 class Reserva_aula extends \app\models\ReservaAula
 {
     public function fields(){
-    return ['id','id_aula','fh_desde','observacion'];
+    return ['id','id_aula','fh_desde','fh_hasta','observacion'];
     }
 
 
